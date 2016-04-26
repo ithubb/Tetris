04820ae96a980b51d13035f25891013dff7b6bdf
Peter Züger <zueger.peter@icloud.com>
2016-04-26 18:07:27 +0200

    Initial Commit

---
+++ Tetris/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tetris
+//
+//  Created by Peter Züger on 26.04.16.
+//  Copyright © 2016 Peter Züger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
